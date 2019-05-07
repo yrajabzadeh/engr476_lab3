5df6014be3b753caafad0ad4dc8adaee35d10056
Yousef Rajabzadeh <>
2019-05-07 13:37:49 -0700

    added map_host.c

---
+++ cmake-build-debug/CMakeFiles/lab3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3.dir/main.c.o"
+  "lab3.pdb"
+  "lab3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
