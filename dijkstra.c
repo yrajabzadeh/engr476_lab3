f0592ac2f4897e0a8df4890980f106a42bc2cb07
yrajabzadeh <20955264+yrajabzadeh@users.noreply.github.com>
2019-05-16 14:40:06 -0700

    final working

--- dijkstra.c
+++ dijkstra.c
@@ -1,63 +1,99 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include<conio.h>
+#include<stdlib.h>
 
 #define INFINITY 9999
 #define MAX 10
-#define N 10
+#define MAX_SIZE 1000
 
-void dijkstra(int** G,int n,int startnode);
-
+void dijkstra(int** G,int n,int startnode,int destination);
 
 int main()
 {
-    int i,j,n=0,u; //matrix G with maximum of ten elements
-        FILE *fp;
+    int i,j,n=0,u,v;
+
     char ch;
-    /*fp = fopen("map.txt", "r");
+    char U;
+    char V;
 
-    while(!feof(fp))
+    FILE *file;
+    file=fopen("map.txt", "r");
+
+    while(!feof(file))
     {
-        ch = fgetc(fp);
-        if (ch == '\n')
+    ch = fgetc(file);
+    if(ch == '\n')
+    {
+        n++;
+    }
+    }
+    fclose(file);
+    file=fopen("map.txt", "r");
+
+    int** G = (int **) malloc(10 * sizeof (int));
+    for(i=0;i<10;++i)
+    G[i]=malloc(8*sizeof(int));
+    for(i = 0; i < n; i++)
         {
-            n++;
-        }
-    }
-    printf("n = %d", n);
-    fclose(fp);*/
-    printf("Enter number of nodes:"); //entering number of elements
-    scanf("%d",&n);
-        fp = fopen("map.txt", "r");
-        int** G = (int **) malloc(10 * sizeof(int));
-    for(i=0; i<10; i++)
-        G[i]=malloc(8* sizeof(int));
-    for(i=0;i<n;i++) {
-        for (j = 0; j < n; j++) {
-            if(!fscanf(fp, "%d", &G[i][j]))
-            break;
-        }
-    }
-    fclose(fp);
+            for(j = 0; j < n; j++)
+            {
 
-    for(i=0;i<n;i++)
-        for (j = 0; j < n; j++){
-            printf("map = %d", G[i][j] );
-        }
+                if (!fscanf(file, "%d", &G[i][j]))
+                break;
+            }
 
-    printf("\nEnter the starting node number:"); //should be starting from 0(A)
-    scanf("%d", &u);
-    dijkstra(G,n,u);
+        }
+    fclose(file);
+    while(1)
+    {
+    printf("\nEnter the source node:");
+    scanf(" %c",&U);
+    if (U =='a')
+        u=0;
+    else if (U =='b')
+        u=1;
+    else if (U =='c')
+        u=2;
+    else if (U =='d')
+        u=3;
+    else if (U =='e')
+        u=4;
+    else if (U =='f')
+        u=5;
+    else if (U =='g')
+        u=6;
+    else if (U =='h')
+        u=7;
+    printf("\nEnter the destination node:");
+    scanf(" %c",&V);
+    if(V =='a')
+        v=0;
+    else if (V =='b')
+        v=1;
+    else if (V =='c')
+        v=2;
+    else if (V =='d')
+        v=3;
+    else if (V =='e')
+        v=4;
+    else if (V =='f')
+        v=5;
+    else if (V =='g')
+        v=6;
+    else if (V =='h')
+        v=7;
+    dijkstraalg(G,n,u,v);
+    }
 
     return 0;
 }
 
-    void dijkstra(int** G,int n,int startnode)
+void dijkstraalg (int** G,int n,int startnode,int destination)
 {
 
     int cost[MAX][MAX],distance[MAX],pred[MAX];
     int visited[MAX],count,mindistance,nextnode,i,j;
-    printf("im here");
+
 /*
 pred[] stores the previous value of each node
 count gives the number of nodes visited
@@ -110,16 +146,16 @@ create the cost matrix
 
 //prints the path and distance of each node
 
-    for(i=0;i<n;i++)
-        if(i!=startnode)
+
+        if(destination!=startnode)
         {
-            printf("\nDistance of node%d=%d",i,distance[i]);
-            printf("\nPath=%d",i);
-            j=i;
+            printf("\nDistance of node%d=%d",destination,distance[destination]);
+            printf("\nPath=%c",destination+97);
+            j=destination;
             do
             {
                 j=pred[j];
-                printf("<-%d",j);
+                printf("<-%c",j+97);
             }
             while(j!=startnode);
         }
